fa023b4793f499a2fee74e1ea5ec3753aa2e763b
Roberto Chambi <rcchambi4@umsa.bo>
2023-04-23 22:24:31 -0400

    básicos de php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$valor = "Título de la página";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HOLA MUNDO</title>
+</head>
+<body>
+  <h1><?php echo $valor; ?></h1>
+</body>
+</html>
